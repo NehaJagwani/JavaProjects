eb23872b4aca3f3f2b8a4eb2fd616074024fb6b1
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 14:27:04 +1000

    added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(2+2);
+        System.out.println("hi, how r u");
         System.out.println(2-2);
         System.out.println(2*2);
         System.out.println(2/2);
