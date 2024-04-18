f26f2ff29e427f410152a382d990dbb44c22389a
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-18 13:33:46 +1000

    hi, pls send

--- src/Ex_11042024/Lab13.java
+++ src/Ex_11042024/Lab13.java
@@ -8,4 +8,5 @@ public class Lab13 {
     //hi, am checking drop-down option of commit and push.
     //checking pull optionnnn
     //checking if the code is able to move to my original Java repo
+    //checking if it can be pulled via the menu options
 }
