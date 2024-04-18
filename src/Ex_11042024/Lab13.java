1f3d945d80b5c48bb5f3f88309cae671ab5d6aa2
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-18 14:03:21 +1000

    ok

--- src/Ex_11042024/Lab13.java
+++ src/Ex_11042024/Lab13.java
@@ -5,6 +5,7 @@ public class Lab13 {
     public static void main(String[] args) {
         System.out.println("neha");
         System.out.println("ok");
+        System.out.println("ok1");
     }
 
 }
