e4488faa0a9e3773406af0d5568edc85c9b60899
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-18 13:38:01 +1000

    add

--- src/Ex_11042024/Lab13.java
+++ src/Ex_11042024/Lab13.java
@@ -2,11 +2,8 @@ package Ex_11042024;
 
 public class Lab13 {
 
+    public static void main(String[] args) {
+        System.out.println("neha");
+    }
 
-    // git -> share theproject
-    // commit (add message)-> push ->
-    //hi, am checking drop-down option of commit and push.
-    //checking pull optionnnn
-    //checking if the code is able to move to my original Java repo
-    //checking if it can be pulled via the menu options
 }
