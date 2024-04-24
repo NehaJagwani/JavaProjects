27f9480bc8d7454e56ffca33e5bb1e8f25892a91
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-24 20:17:31 +1000

    homework23rdapril

--- src/Homework23rdApril/Lab76_FizzBuzz.java
+++ src/Homework23rdApril/Lab76_FizzBuzz.java
@@ -9,11 +9,11 @@ public class Lab76_FizzBuzz {
             {
                 System.out.printf("%d: The number is both a multiple of 3 and 5: Hurray, It's a FIZZBUZZ. \n", i1);
             }
-            else if ((i1%3==0) && (i1%5!=0)) /*3,6,9,12,18,21,24,27*/
+            else if ((i1%3==0)) /*3,6,9,12,18,21,24,27*/
             {
                 System.out.printf("%d: The number is a multiple of 3: Wow, It's a FIZZ. \n", i1);
             }
-            else if ((i1%5==0) && (i1%3!=0)) //5,10,20,25,35,40,50
+            else if ((i1%5==0)) //5,10,20,25,35,40,50
             {
                 System.out.printf("%d: The number is a multiple of 5: Omg, it's a BUZZ. \n", i1);
             }
